e22cd442940b9e8aedf9883e25fcdca9c8d504ea
OGSegu <krivitskij.vv@edu.spbstu.ru>
2022-12-12 22:13:25 +0300

    REST API #12

---
+++ src/main/java/service/PersistenceService.java
@@ -0,0 +1,35 @@
+package service;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.tika.utils.StringUtils;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class PersistenceService {
+
+    private static final Path UPLOADED_DIR = Path.of("./data");
+
+    public PersistenceService() throws IOException {
+        if (Files.notExists(UPLOADED_DIR)) {
+            Files.createDirectories(UPLOADED_DIR);
+        }
+    }
+
+    public File storeMultiPartFile(MultipartFile multipartFile) throws IOException {
+        long timestamp = System.currentTimeMillis();
+        String fileName = multipartFile.getOriginalFilename();
+        if (StringUtils.isBlank(fileName)) {
+            fileName = multipartFile.getName();
+        }
+        Path file = UPLOADED_DIR.resolve(fileName + "_" + timestamp);
+        Files.createFile(file);
+        multipartFile.transferTo(file);
+        return file.toFile();
+    }
+
+
+}
