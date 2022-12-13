ab6cc5b7bf3e39ffca15fc9608e92ba61e528b0b
OGSegu <krivitskij.vv@edu.spbstu.ru>
2022-12-13 21:37:40 +0300

    Added lucene core

---
+++ src/main/java/lucene/LuceneService.java
@@ -0,0 +1,18 @@
+package lucene;
+
+import org.apache.lucene.search.Query;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public interface LuceneService {
+
+    String TITLE_FIELD = "title";
+    String BODY_FIELD = "body";
+
+    boolean write(File file);
+    List<String> search(Query query);
+    Path getIndexDir();
+}
